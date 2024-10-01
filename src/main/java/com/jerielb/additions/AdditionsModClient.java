f36e05411cbf4195f539a205633215e876a05881
jerielB <jerielbongco98@gmail.com>
2024-10-01 12:17:20 +1000

    initial commit

---
+++ src/main/java/com/jerielb/additions/AdditionsModClient.java
@@ -0,0 +1,10 @@
+package com.jerielb.additions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdditionsModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
